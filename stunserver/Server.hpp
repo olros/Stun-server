f67a0a919061a385832ea6fd26808746e2fa2dfb
Hermann Elton <60223374+Her0elt@users.noreply.github.com>
2021-03-17 09:48:59 +0100

    Feat(base-stun): Implemented base stun server logic (#4)
    * init
    
    * getting ip and port
    
    * no longer that hard coded
    
    * builder pattern
    
    * done
    
    * gitignore

--- stunserver/Server.hpp
+++ stunserver/Server.hpp
@@ -5,6 +5,8 @@
 #include "Workers.hpp"
 #include <unistd.h>
 #include <arpa/inet.h>
+#include "stuntypes.h"
+#include "ResponseBuilder.hpp"
 
 class Server {
 private:
@@ -18,6 +20,7 @@ public:
     bool startServer();
     void closeServer();
     ~Server();
+    
 };
 
 Server::Server() {}
@@ -34,50 +37,48 @@ bool Server::startServer() {
     keep_going = true;
     struct sockaddr_in server, client;
     struct sockaddr_in6 server_ip6, client_ipv6;
-    const int bufferSize =1024;
-    char* buffer = new char[bufferSize]();
-    int socket_fd = socket(AF_INET6, SOCK_DGRAM, 0);
+    const int bufferSize =256;
+    int socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
     if(socket_fd == -1) return false;
 
 
     //Ipv6
-    server_ip6.sin6_family=AF_INET6;
-    server_ip6.sin6_port=htons(this->port);
-    server_ip6.sin6_addr=in6addr_any;
-    /*//Ipv4
+    // server_ip6.sin6_family=AF_INET6;
+    // server_ip6.sin6_port=htons(this->port);
+    // server_ip6.sin6_addr=in6addr_any;
+    //Ipv4
     server.sin_port = htons(this->port);
     server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_family = AF_INET;*/
+    server.sin_family = AF_INET;
 
 
-    if(bind(socket_fd, (const struct sockaddr *)&server_ip6, sizeof(server_ip6)) < 0){
+    if(bind(socket_fd, reinterpret_cast<struct sockaddr*>(&server), sizeof(server)) < 0){
         close(socket_fd);
         return false;
     }
 
     int n;
-    socklen_t length = sizeof(client_ipv6);
+    socklen_t length = sizeof(client);
 
     while (keep_going) {
-        n = recvfrom(socket_fd, (char *) buffer, bufferSize, MSG_WAITALL, (struct sockaddr *) &client_ipv6,
+        std::cout<<"We are inn boyzz!!!!!"<<std::endl;
+        unsigned char buffer[bufferSize];
+        n = recvfrom(socket_fd, buffer, sizeof(buffer), MSG_WAITALL, reinterpret_cast<struct sockaddr*>(&client),
                      &length);
-
         //TODO remove logging of ipv4 and ipv6 addresses
         char ip4[16];
-        inet_ntop(AF_INET, &client_ipv6.sin6_addr, ip4, sizeof(ip4));
-        std::cout << "v4: " << ip4 << " : " << ntohs(client_ipv6.sin6_port) << std::endl;
+        inet_ntop(AF_INET, &client.sin_addr, ip4, sizeof(ip4));
+        std::cout << "v4: " << ip4 << " : " << ntohs(client.sin_addr.s_addr) << std::endl;
 
-        char ip6[16];
-        inet_ntop(AF_INET6, &client_ipv6.sin6_addr, ip6, sizeof(ip6));
-        std::cout << "v6: " << ip6 << " : " << ntohs(client_ipv6.sin6_port) << std::endl;
+        // char ip6[16];
+        // inet_ntop(AF_INET6, &client_ipv6.sin6_addr, ip6, sizeof(ip6));
+        // std::cout << "v6: " << ip6 << " : " << ntohs(client_ipv6.sin6_port) << std::endl;
 
-        buffer[n] = '\0';
-
-        //TODO Parse ip-address and use asio
-
-        //2048 is equal to MSG_CONFIRM which the IDE does not recognize
-        sendto(socket_fd, (const char *) buffer, strlen(buffer), 2048, (const struct sockaddr *) &client_ipv6, sizeof(client_ipv6));
-        buffer = new char[bufferSize]();
+        ResponseBuilder builder = ResponseBuilder(true, (STUNIncommingHeader*)buffer, client);
+ 
+    
+        sendto(socket_fd, builder.buildSuccessResponse().getResponse(), sizeof(struct STUNResponseIPV4), 
+            MSG_CONFIRM, (const struct sockaddr *) &client, sizeof(client));
     }
     close(socket_fd);
     return true;
@@ -91,3 +92,11 @@ void Server::closeServer() {
 Server::~Server() {
     delete event_loop;
 }
+
+
+
+
+
+
+
+
---
+++ stunserver/SuccessResponseBuilder.hpp
@@ -0,0 +1,112 @@
+#ifndef SuccessResponseBuilder_hpp
+#define SuccessResponseBuilder_hpp
+
+#include <iostream>
+#include "stuntypes.h"
+#include <arpa/inet.h>
+
+// TODO Fix the XOR function to correctly XOR the port and IP.
+// TODO Maybe add a isIPv4 object attribute so that we dont need to check for it,
+// but this depends on how we want to use SuccessResponseBuilder in the server class
+
+
+#define SuccessCode 0x0001
+class SuccessResponseBuilder{
+    private:
+        struct STUNResponse* res;
+        void setPort(uint8_t *port);
+        void setIP(uint8_t *ip, bool isIPv4);
+    public:
+        long getlength();
+        SuccessResponseBuilder();
+        struct STUNResponse* getResponse();
+        SuccessResponseBuilder&  setStunSuccessHeaders(struct STUNIncommingHeader* inc);
+        SuccessResponseBuilder&  setlength(bool isIPv4);
+        SuccessResponseBuilder&  setAttType(int type);
+        SuccessResponseBuilder&  setAttLength(bool isIPv4);
+        SuccessResponseBuilder&  setProtocol( bool isIPv4);
+        SuccessResponseBuilder&  setPadding( int size);
+        SuccessResponseBuilder& XORAttributes(in_addr_t &ip,in_port_t &port, bool isIPv4);
+};
+
+SuccessResponseBuilder::SuccessResponseBuilder(){
+    res = (STUNResponse*) malloc(sizeof(struct STUNResponse));
+}
+
+struct STUNResponse* SuccessResponseBuilder::getResponse(){
+    return res;
+}
+
+SuccessResponseBuilder&  SuccessResponseBuilder::setStunSuccessHeaders(struct STUNIncommingHeader* inc){
+    res->type = htons(SuccessCode);
+    for(int i = 0; i < identifier_size; i++){
+        res->identifier[i] = inc->identifier[i];
+    }
+    return *this;
+}
+
+SuccessResponseBuilder&  SuccessResponseBuilder::setlength(bool isIPv4){
+    res->length = htons(isIPv4? 12: 24);
+    return *this;
+
+}
+
+SuccessResponseBuilder&  SuccessResponseBuilder::setAttType(int type){
+    res->atttype= htons(XOR_MAPPED_ADdRESSS);
+    return *this;
+}
+SuccessResponseBuilder&  SuccessResponseBuilder::setAttLength(bool isIPv4){
+    res->attlength = htons(isIPv4 ? 8 : 20);
+    return *this;
+
+}
+
+
+SuccessResponseBuilder&  SuccessResponseBuilder::setProtocol(bool isIPv4){
+    res->protocol = isIPv4 ? IPv4_PROTOCOL_VALUE : IPv6_PROTOCOL_VALUE; 
+    return *this;
+
+}
+
+SuccessResponseBuilder&  SuccessResponseBuilder::setPadding(int size){
+    res->padding = ((size_t )res & 1) ? 0x01 : 0x00;
+    return *this;
+}
+
+SuccessResponseBuilder& SuccessResponseBuilder::XORAttributes(in_addr_t &ip ,in_port_t &port, bool isIPv4){
+    uint8_t* pPort = (uint8_t*)&(port);
+    uint8_t* pIP = (uint8_t*)&(ip);
+
+    pPort[0] = pPort[0] ^ res->identifier[0];
+    pPort[1] = pPort[1] ^ res->identifier[1];
+
+    
+    for (size_t i = 0; i < (isIPv4? 4: 16); i++){
+        pIP[i] = pIP[i] ^ res->identifier[i];
+    }
+    setPort(pPort);
+    setIP(pIP, isIPv4);
+
+    return *this;
+}
+
+void SuccessResponseBuilder::setPort(uint8_t *port){
+    for (size_t i = 0; i < 2; i++){
+        res->port[i] = port[i];
+    }
+    
+}
+
+void SuccessResponseBuilder::setIP(uint8_t *ip, bool isIPv4){
+    for(int i = 0; i < (isIPv4? 4: 16); i++){
+        res->ip[i] = ip[i];
+    }
+}
+
+long SuccessResponseBuilder::getlength(){
+    auto length = sizeof(res);
+    return length;
+}
+#endif
+
+
