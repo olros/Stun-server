83774700d41942cc2d45c1e57726c42d39a49060
Martin Hagen <60466242+hamasl@users.noreply.github.com>
2021-03-22 14:33:08 +0100

    refactor(server-tcp): implemented both udp and tcp for the server (#21)
    * Started working on adding tcp to the server
    
    * Wierd bug does not work
    
    * For testing, always send correct answer on tcp, remove this before merging
    
    * Made tcp and udp work together
    
    * CD
    
    * Changed stun server address in frontend
    
    Co-authored-by: Martin Sind Hagen <60466242+MartinSlindHagen@users.noreply.github.com>

--- stunserver/Server.hpp
+++ stunserver/Server.hpp
@@ -1,22 +1,45 @@
 #include <iostream>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <netinet/in.h>
+#include <netdb.h>
 #include <string.h>
+#include <cstring>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include "Workers.hpp"
 #include "stuntypes.h"
 #include "ResponseBuilder.hpp"
+#include <errno.h>
+
+
+#define BUFFER_SIZE 256
+#define BACKLOG 5
+//Added enum for future possibilities of implementing TLS
+enum SocketType {
+    UDP, TCP
+};
+
 
 class Server {
 private:
     int port;
     Workers *event_loop;
     bool keep_going;
+    int socket_fd;
+    SocketType socket_type;
+    struct addrinfo *result;
+
+    bool init_listening_socket();
+
+    bool handle_udp(ResponseBuilder &builder, sockaddr_in &client, unsigned char (&buffer)[BUFFER_SIZE], socklen_t &length);
+
+    bool handle_tcp(ResponseBuilder &builder,sockaddr_in &client, unsigned char (&buffer)[BUFFER_SIZE], socklen_t &length);
+
 public:
     Server();
 
-    Server(int port);
+    Server(int socket_port, SocketType sock_type);
 
     bool startServer();
 
@@ -27,65 +50,98 @@ public:
 
 Server::Server() {}
 
-Server::Server(int port) {
-    this->port = port;
+Server::Server(int socket_port, SocketType sock_type) {
+    this->port = socket_port;
     this->event_loop = new Workers(1);
+    this->socket_type = sock_type;
 }
 
-//NB! Max 5 connections per socket, when using listen method
-//NB! Implement sultions that don differentiate between ipv6 and ipv4, maybe one thread for each, and take in the protocol in constructor
-bool Server::startServer() {
-    event_loop->start();
-    keep_going = true;
+bool Server::init_listening_socket() {
+    this->socket_fd = socket(AF_INET, this->socket_type == TCP ? SOCK_STREAM : SOCK_DGRAM, 0);
+    if (this->socket_fd == -1){
+        std::cerr << "socket() failed: " << strerror(this->socket_fd) << std::endl;
+        return false;
+    }
+    struct sockaddr_in sock_addr;
+    sock_addr.sin_addr.s_addr = INADDR_ANY;
+    sock_addr.sin_port = htons(port);
+    sock_addr.sin_family = AF_INET;
 
-    struct sockaddr_in server;
-    struct sockaddr_in6 server_ip6;
-    const int bufferSize = 256;
-    int socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (socket_fd == -1) return false;
-
-    //Ipv6
-    // server_ip6.sin6_family=AF_INET6;
-    // server_ip6.sin6_port=htons(this->port);
-    // server_ip6.sin6_addr=in6addr_any;
-
-    //Ipv4
-    server.sin_port = htons(this->port);
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_family = AF_INET;
-
-
-    if(bind(socket_fd, (struct sockaddr*)(&server), sizeof(server)) < 0){
+    int exit_code;
+    if((exit_code = bind(socket_fd, (struct sockaddr*)(&sock_addr), sizeof(sock_addr))) < 0){
+        std::cerr << "bind() failed with exit code: " << strerror(exit_code) << std::endl;
         close(socket_fd);
         return false;
     }
+    return true;
+}
 
-    while (keep_going) {
-        std::cout <<"uuuu"<<std::endl;
-        struct sockaddr_in6 client_ipv6;
-        struct sockaddr_in client;
-        socklen_t length = sizeof(client);
-        unsigned char buffer[bufferSize];
-        int n = recvfrom(socket_fd, buffer, sizeof(buffer), 
-        MSG_WAITALL, (struct sockaddr*)(&client),&length);
-        ResponseBuilder builder;
-        bool isError = false;
-        event_loop->post_after([ &server, &client, &socket_fd, &buffer, &builder, &isError] {
-            if(isError || builder.isError()) sendto(socket_fd, builder.buildErrorResponse(400, "Something went wrong!?").getResponse(),sizeof(struct StunErrorResponse), MSG_CONFIRM, 
-                (const struct sockaddr *) &client, sizeof(client));
-
-            else sendto(socket_fd, builder.buildSuccessResponse().getResponse(), sizeof(struct STUNResponseIPV4),
-                       MSG_CONFIRM, (const struct sockaddr *) &client, sizeof(client));
-    
-        }, [&builder, &buffer, &client, &isError, &n] {
-            builder = ResponseBuilder(true, (STUNIncommingHeader *) buffer, client);
-            isError =((buffer[0] >> 6) & 3) != 0 || n<20;
-        });
-
-        char ip4[16];
-        inet_ntop(AF_INET, &client.sin_addr, ip4, sizeof(ip4));
+bool Server::handle_udp(ResponseBuilder &builder, sockaddr_in &client, unsigned char (&buffer)[BUFFER_SIZE], socklen_t &length) {
+    bool isError = false;
+    int n = recvfrom(socket_fd, buffer, sizeof(&buffer),
+                     MSG_WAITALL, (struct sockaddr *) (&client), &length);
+    if (n == -1) {
+        std::cerr << "recvfrom() failed: " << strerror(n) << std::endl;
+        return false;
     }
-    close(socket_fd);
+    event_loop->post_after([&client, this, &buffer, &builder, &isError] {
+        if (isError || builder.isError())
+            sendto(this->socket_fd, builder.buildErrorResponse(400, "Something went wrong!?").getResponse(),
+                   sizeof(struct StunErrorResponse), MSG_CONFIRM,
+                   (const struct sockaddr *) &client, sizeof(client));
+
+        else
+            sendto(this->socket_fd, builder.buildSuccessResponse().getResponse(), sizeof(struct STUNResponseIPV4),
+                   MSG_CONFIRM, (const struct sockaddr *) &client, sizeof(client));
+    }, [&builder, &buffer, &client, &isError, &n] {
+        builder = ResponseBuilder(true, (STUNIncommingHeader *) buffer, client);
+        isError = ((buffer[0] >> 6) & 3) != 0 || n < 20;
+    });
+    return true;
+}
+
+bool Server::handle_tcp(ResponseBuilder &builder, struct sockaddr_in &client, unsigned char (&buffer)[BUFFER_SIZE], socklen_t &length) {
+    bool isError = false;
+    int client_socket_fd = accept(socket_fd, (struct sockaddr *) &client, &length);
+    if (client_socket_fd == -1) return false;
+    event_loop->post_after([&builder, &client_socket_fd, &isError]{
+        if (isError || builder.isError())
+            send(client_socket_fd, builder.buildErrorResponse(400, "Something went wrong!?").getResponse(),
+                   sizeof(struct StunErrorResponse), MSG_CONFIRM);
+
+        else
+            send(client_socket_fd, builder.buildSuccessResponse().getResponse(), sizeof(struct STUNResponseIPV4),
+                   MSG_CONFIRM);
+        close(client_socket_fd);
+    }, [&builder, &client_socket_fd, &buffer, &isError, client]{
+        int n = recv(client_socket_fd, buffer, BUFFER_SIZE,0);
+        if(n == -1) std::cerr << "recv() failed: " << n << std::endl;
+        builder = ResponseBuilder(true, (STUNIncommingHeader *) buffer, client);
+        isError = ((buffer[0] >> 6) & 3) != 0 || n < 20;
+    });
+
+    return true;
+}
+
+bool Server::startServer() {
+
+    event_loop->start();
+    keep_going = true;
+
+    if (!init_listening_socket()) return false;
+    if (socket_type == TCP) listen(socket_fd, BACKLOG);
+    while (keep_going) {
+        struct sockaddr_in client;
+        unsigned char buffer[BUFFER_SIZE];
+        socklen_t length = sizeof(client);
+        ResponseBuilder builder;
+
+        //Should not matter if we send copies because we will not manipulate parameters here anymore
+        socket_type == TCP ? handle_tcp(builder, client, buffer, length) : handle_udp(builder, client,
+                                                                                                  buffer, length);
+        char ip4[16];
+        inet_ntop(AF_INET, &(((const struct sockaddr_in *)&client)->sin_addr), ip4, sizeof(ip4));
+    }
     return true;
 }
 
@@ -93,6 +149,8 @@ void Server::closeServer() {
     event_loop->stop();
     event_loop->join();
     keep_going = false;
+    freeaddrinfo(result);
+    close(socket_fd);
 }
 
 Server::~Server() {
--- stunserver/main.cpp
+++ stunserver/main.cpp
@@ -2,10 +2,20 @@
 #include "Server.hpp"
 #include <thread>
 
+#define SOCKET_PORT 3478
 int main() {
     std::cout << "The server is starting, hang on a second..." << std::endl;
-    Server server(80);
-    std::cout << server.startServer() << std::endl;
+    Server tcp_server(SOCKET_PORT, SocketType::TCP);
+    Server udp_server(SOCKET_PORT, SocketType::UDP);
+    std::thread tcp([&tcp_server]{
+        tcp_server.startServer();
+    });
+    std::thread udp([&udp_server]{
+        udp_server.startServer();
+    });
     std::cout << "Stun server started!" << std::endl;
+
+    tcp.join();
+    udp.join();
     return 0;
 }
